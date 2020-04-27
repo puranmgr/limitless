5b3e8013fbe3c42a8ceb5ecf7f43d10cba9cd72e
puranmgr <puranmgr@gmail.com>
2020-04-27 13:12:37 +0100

    Updates

---
+++ precache-manifest.bde1275ca72e66d54631b2c0f4ef2322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc702d9518f2b63c1a68eddc0360a58",
+    "url": "/limitless/index.html"
+  },
+  {
+    "revision": "b7a144627662d4b9b583",
+    "url": "/limitless/static/css/main.f8ca13c6.chunk.css"
+  },
+  {
+    "revision": "08294b5be487e1cfb4a3",
+    "url": "/limitless/static/js/2.40f03d5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/limitless/static/js/2.40f03d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a144627662d4b9b583",
+    "url": "/limitless/static/js/main.d6dc3d60.chunk.js"
+  },
+  {
+    "revision": "ae993a009d980ff11397",
+    "url": "/limitless/static/js/runtime-main.546c9e94.js"
+  }
+]);
